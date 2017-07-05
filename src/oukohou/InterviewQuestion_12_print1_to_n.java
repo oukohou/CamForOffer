15f85cf98ce023bc716c1eb8b7daba38f5e2d70d
oukohou <oukohou@outlook.com>
2017-07-05 21:26:44 +0800

    add sereral interview questions.

---
+++ src/oukohou/InterviewQuestion_12_print1_to_n.java
@@ -0,0 +1,15 @@
+package oukohou;
+
+/**
+ * Created by oukohou on 2017/7/5.
+ * If this runs wrong, don't ask me, I don't know why;
+ * If this runs right, thank god, and I don't know why.
+ * Maybe the answer, my friend, is blowing in the wind.
+ */
+public class InterviewQuestion_12_print1_to_n {
+
+
+    public static void main(String[] args) {
+
+    }
+}
