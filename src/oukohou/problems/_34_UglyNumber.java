20b17a04dd8538444f9502b52b4317d7ccf874cc
oukohou <oukohou@outlook.com>
2017-09-11 09:22:18 +0800

    add  some more problems .

---
+++ src/oukohou/problems/_34_UglyNumber.java
@@ -0,0 +1,43 @@
+package oukohou.problems;
+
+/**
+ * Created by oukohou on 2017/9/9.
+ * If this runs wrong, don't ask me, I don't know why;
+ * If this runs right, thank god, and I don't know why.
+ * Maybe the answer, my friend, is blowing in the wind.
+ */
+public class _34_UglyNumber {
+    public static int getUglyNumber(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        int[] uglyNumbers = new int[n];
+        uglyNumbers[0] = 1;
+        int count2 = 0;
+        int count3 = 0;
+        int count5 = 0;
+        for (int i = 1; i < n; i++) {
+            uglyNumbers[i] = getMinNumber(uglyNumbers[count2] * 2, uglyNumbers[count3] * 3, uglyNumbers[count5] * 5);
+
+            while (uglyNumbers[count2] * 2 <= uglyNumbers[i]) count2++;
+            while (uglyNumbers[count3] * 3 <= uglyNumbers[i]) count3++;
+            while (uglyNumbers[count5] * 5 <= uglyNumbers[i]) count5++;
+        }
+        return uglyNumbers[n - 1];
+
+    }
+
+    private static int getMinNumber(int count2, int count3, int count5) {
+        count2 = count2 > count3 ? count3 : count2;
+        count2 = count2 > count5 ? count5 : count2;
+        return count2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getUglyNumber(18));
+    }
+}
